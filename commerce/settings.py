7a226865d6e67f974a51e0de93e9b8c7e84ff32b
lucasbashir <lucassamson24@gmail.com>
2023-01-25 03:39:15 +0100

    Revert "readme"
    This reverts commit 4e5b420135f775ad35d33ea11b20b675ede281e9.

--- commerce/settings.py
+++ commerce/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
-from decouple import config
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,4 +128,3 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
