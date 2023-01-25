6a8e8246e5c4d0dddc4451e74f1a57c0d07f3a5a
Lucas <lucas@DESKTOP-MRGN6VK.localdomain>
2023-01-25 03:13:45 +0100

    installed psycog2

--- auctions/views.py
+++ auctions/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+import psycopg2
 from django.urls import reverse
 
 
