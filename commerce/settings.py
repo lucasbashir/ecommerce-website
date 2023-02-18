41ab2e2a5235554f176d1527ec735a5079f34d46
lucasbashir <lucassamson24@gmail.com>
2023-02-18 22:38:49 +0100

    Revert "tweaked app for heroku"
    This reverts commit f97816baf5749e49ca727096ba4cf8c15f19c9e1.

--- commerce/settings.py
+++ commerce/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,7 +121,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-
-
-django_heroku.settings(locals())
