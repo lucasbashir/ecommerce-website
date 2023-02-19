296629af6723af53e7dbcfd856241f9db2742b8b
lucasbashir <37589048+lucasbashir@users.noreply.github.com>
2023-02-19 01:04:24 +0100

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from django.core.wsgi import get_wsgi_application
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commerce.settings")
 
 
 # Quick-start development settings - unsuitable for production
